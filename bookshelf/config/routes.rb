f41fd48e42e9fae951fdd5480d9db6004c289ab9
Sumit Srivastava <srivassumit@gmail.com>
2017-02-07 18:39:52 -0500

    updated routes.rb file

--- bookshelf/config/routes.rb
+++ bookshelf/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :books do |book|
+  resources :books do
     resources :comments, :only => [:create]
   end
 end
