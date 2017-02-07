827ca64e76b774978a5535d64d471c34d3d1ef9e
Sumit Srivastava <srivassumit@gmail.com>
2017-02-07 16:13:11 -0500

    added pagination

--- bookshelf/app/controllers/books_controller.rb
+++ bookshelf/app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 	# notice we've defined a method called index for a BooksController instance. We tie this together with routes
 	def index
-		@books = Book.all # instance variables are prefixed with an @. If we said books = Book.all, we wouldn't be able to access books in the template
+		@books = Book.paginate(page: params[:page], per_page: 10) # instance variables are prefixed with an @. If we said books = Book.all, we wouldn't be able to access books in the template
 	end
 end
